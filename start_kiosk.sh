ff2f841e84a3988cb613bf834aeb2fff2e13c573
Philip Rodriguez <ptr5201@gmail.com>
2026-03-15 20:19:02 -0400

    added startup script to start weather kiosk

---
+++ start_kiosk.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# 1. Move to the project directory
+cd /home/ptr5201/projects/weather_kiosk
+
+# 2. Run the app using the direct path to the VENV python
+# The '&' puts it in the background so the script can continue
+./venv/bin/python3 app.py &
+
+# 3. Wait for the server to bind to the port
+sleep 5
+
+# 4. Launch Chromium in kiosk mode
+chromium-browser --kiosk --incognito --noerrdialogs --disable-infobars http://localhost:5000
